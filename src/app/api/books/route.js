9f29d3f65d516da6c6bc2f0faaa839c2aea8063c
aniTech-addict <kirti7666anand15@gmail.com>
2025-10-28 12:09:26 +0530

    feat(api): implement getBooks controller and error handling

--- src/app/api/books/route.js
+++ src/app/api/books/route.js
@@ -1,4 +1,23 @@
-import BOOK from
-export async function GET(req) {
+import BOOK from "@/Lib/Models/Books.model";
+import { getBooks } from "@/Lib/Controllers/book.controller.js";
 
+export async function GET(req) {
+    try{
+        const response = await getBooks();
+        // response.data: {Object} Books
+        return new Response(JSON.stringify({data: response.data}), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+    } catch (err) {
+        console.log("⚠️ Error encounter in Books route",err);
+        return new Response(JSON.stringify({error: "Internal Server Error"}), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+    }
 }

079fbaf432df70a4b3928adc9fa47360412a246d
aniTech-addict <kirti7666anand15@gmail.com>
2025-10-28 08:10:01 +0530

    Form creation using react-form with form validation checks

--- src/app/add/page.jsx
+++ src/app/add/page.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+"use client"
+import {useForm} from "react-hook-form"
+import { useState, useEffect } from "react";
+import { refresh } from "next/cache";
 
-const page = () => {
+const AddPage = () => {
+
+  const {register, handleSubmit, formState: {errors}} = useForm(
+    {
+      defaultValues:{
+        title: "",
+        author: "",
+        genre: "",
+        publicationDate: ""
+      }
+    }
+  ) 
+
+  async function pushInfo(data){
+    await axios({
+      method: "POST",
+      url: "http://localhost:3000/api/add",
+      data: data
+    })
+  }
+  
+  
   return (
-    <div>page</div>
+    <div>
+      <form onSubmit={handleSubmit((data)=>{
+        pushInfo(data);
+        
+      })}
+      className="flex width-full justify-center items-center"
+      >
+        <div className="flex flex-col">
+          <div className="flex flex-row gap-3 mt-2">
+            <p>Title</p>
+            <input {...register("title",{required:"Required Field"})}/>
+            <p className="text-red-400 text-sm">{" *"} {errors.title?.message}</p>
+          </div>
+
+          <div className="author flex flex-row gap-3 mt-2">
+            <p>Author</p>
+            <input {...register("author",{required:"Required Field"})}/>
+            <p className="text-red-400 text-sm">{" *"} {errors.author?.message}</p>
+          </div>
+
+          <div className="genre flex flex-row gap-3 mt-2">
+            <p>Genre</p>
+            <input {...register("genre",{required:"Required Field"})}/>
+            <p className="text-red-400 text-sm">{" *"} {errors.genre?.message}</p>
+          </div>
+
+          <div className="publicationDate flex flex-row gap-3 mt-2">
+            <p>Publication Date</p>
+            <input {...register("publicationDate",{required:"Required Field"})}/>
+            <p className="text-red-400 text-sm">{" *"} {errors.publicationDate?.message}</p>
+          </div>
+
+          <button type="submit"
+          className="bg-amber-300 mt-3 text-black hover:text-gray-800 hover:bg-amber-200 hover:tracking-wider" >
+            Submit
+          </button>  
+        </div>
+        
+      </form>
+    </div>
   )
 }
 
-export default page
+export default AddPage

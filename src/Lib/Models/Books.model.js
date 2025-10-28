20af1713ee3ae4d2c909e0d960a3660313bc1826
aniTech-addict <kirti7666anand15@gmail.com>
2025-10-28 09:53:37 +0530

    Model craetion for Books MetaData

---
+++ src/Lib/Models/Books.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    genre:{
+        type:String,
+        required:true
+    },
+    publicationDate:{
+        type:Date,
+        required:true
+    }
+
+},{timestamps:true})
+
+export default Book = mongoose.model("Book", BookSchema)
